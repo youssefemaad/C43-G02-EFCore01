65b9f5535aa4fb322dcbdc5848f3df39d70ba17f
youssefemaad <eyoussef228@gmail.com>
2025-02-19 20:20:53 +0200

    new

---
+++ Assignement_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assignement_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
